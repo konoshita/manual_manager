9b127aa8b1dc27cf706e2c1e90b11bfd8f79d50d
konoshita <minglicangzhi@gmail.com>
2024-01-12 18:19:49 +0900

    driveの画面修正

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,10 @@ module SampleManual
       g.helper false
       g.test_framework false
     end
-    
+    config.i18n.available_locales = %i[ja en]
+    config.i18n.default_locale = :ja
+
+    # 複数のロケールファイルが読み込まれるようpathを通す
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
