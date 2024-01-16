e79fda88de7938c487c10f7492c1c306ccab350f
konoshita <minglicangzhi@gmail.com>
2024-01-16 22:49:00 +0900

    ユーザーの論理削除

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     root to: 'dashboards#index'
     resources :users, only: %i[index show edit update delete]
     resources :manuals, only: %i[index new create edit update destroy]
+    patch "withdrawal/:id" => "users#withdrawal", as: "withdrawal"
   end
   resources :manuals, only: %i[index new ]
   root "manuals#index"
