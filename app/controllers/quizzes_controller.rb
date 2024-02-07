29851c30475ae8db1613f98f7f35493de5669987
konoshita <minglicangzhi@gmail.com>
2024-02-07 23:32:28 +0900

    一般ユーザービューでクイズ機能が確認できる’ 7

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -8,6 +8,8 @@ class QuizzesController < ApplicationController
 
   # GET /quizzes/1 or /quizzes/1.json
   def show
+    @question = Question.new
+    @questions = @quiz.questions
   end
 
   # GET /quizzes/new
@@ -15,46 +17,6 @@ class QuizzesController < ApplicationController
     @quiz = Quiz.new
   end
 
-  # GET /quizzes/1/edit
-  def edit
-  end
-
-  # POST /quizzes or /quizzes.json
-  def create
-    @quiz = Quiz.new(quiz_params)
-
-    respond_to do |format|
-      if @quiz.save
-        format.html { redirect_to quiz_url(@quiz), notice: "Quiz was successfully created." }
-        format.json { render :show, status: :created, location: @quiz }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @quiz.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /quizzes/1 or /quizzes/1.json
-  def update
-    respond_to do |format|
-      if @quiz.update(quiz_params)
-        format.html { redirect_to quiz_url(@quiz), notice: "Quiz was successfully updated." }
-        format.json { render :show, status: :ok, location: @quiz }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @quiz.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /quizzes/1 or /quizzes/1.json
-  def destroy
-    @quiz.destroy
-    respond_to do |format|
-      format.html { redirect_to quizzes_url, notice: "Quiz was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
