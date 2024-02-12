45d29d22ad80c98944d60e1ac0dc107d61a7e8ba
konoshita <minglicangzhi@gmail.com>
2024-02-12 18:44:52 +0900

    devise_invitableによる招待機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  rescue_from ActiveRecord::RecordNotFound, with: :render404
   include Pundit
   before_action :configure_permitted_parameters, if: :devise_controller?
   layout :layout_by_resource
+
   protected
 
   def after_sign_out_path_for(resource_or_scope)
@@ -14,6 +16,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :email, :name, :password])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password, :role,])
     devise_parameter_sanitizer.permit(:account_update, keys: [:role, :email, :name, :password])
+    devise_parameter_sanitizer.permit(:invite) { |u| u.permit(:email, :name) }
+    devise_parameter_sanitizer.permit(:accept_invitation) { |u| u.permit(:password, :password_confirmation, :invitation_token, :name) }
+  end
+
+  def render404(error = nil)
+    Rails.logger.error("❌#{error.message}") if error
+    render template: 'errors/render404', layout: 'devise', status: :not_found
   end
 
   private
