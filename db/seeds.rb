d23b28ab4f6c60fec542118c922109f8c7a71de9
konoshita <minglicangzhi@gmail.com>
2024-02-16 09:51:27 +0900

    デプロイ

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,24 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 User.create!(
-  name: '管理者',
-  email: 'admin@example.com',
-  password: 'password',
-  password_confirmation: 'password',
+  name: '此下明里',
+  email: 'minglicangzhi@gmail.com',
+  password: 'aki00aka',
+  password_confirmation: 'aki00aka',
   role: 1
 )
-10.times do |index|
-  User.create!(
-      name: "ユーザー#{index}",
-      email: "test#{index}@test.com",
-      password: 'aaaaaaa',
-      password_confirmation: 'aaaaaaa',
-  )
-end
-20.times do |index|
-  Manual.create!(
-      user: User.offset(rand(User.count)).first,
-      title: "タイトル#{index}",
-      category_id: Category.offset(rand(Category.count)).first,
-  )
-end
+# 10.times do |index|
+#   User.create!(
+#       name: "ユーザー#{index}",
+#       email: "test#{index}@test.com",
+#       password: 'aaaaaaa',
+#       password_confirmation: 'aaaaaaa',
+#   )
+# end
+# 20.times do |index|
+#   Manual.create!(
+#       user: User.offset(rand(User.count)).first,
+#       title: "タイトル#{index}",
+#       category_id: Category.offset(rand(Category.count)).first,
+#   )
+# end
