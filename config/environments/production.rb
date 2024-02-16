5ec27d1e9ef2e5171189771e12aedc7b7502fb12
konoshita <minglicangzhi@gmail.com>
2024-02-16 21:13:02 +0900

    デプロイ4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,22 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
+  config.action_mailer.default_url_options = {  :host => 'https://bi-bo-u4dr.onrender.com' }
+  #送信方法を指定（この他に:sendmail/:file/:testなどがあります)
+  config.action_mailer.delivery_method = :smtp
+  #送信方法として:smtpを指定した場合は、このconfigを使って送信詳細の設定を行います
+  config.action_mailer.smtp_settings = {
+    #gmail利用時はaddress,domain,portは下記で固定
+    address:"pharma88a@gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    #gmailのユーザアカウント（xxxx@gmail.com)※念のため、credentials.yml.enc行き
+    user_name: Rails.application.credentials.gmail[:user_name],
+    #gmail２段階認証回避のためにアプリケーションでの利用パスワードを取得、必ずcredentials.yml.endに設定を！！
+    password: Rails.application.credentials.gmail[:password],
+    #パスワードをBase64でエンコード
+    authentication: :login
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
