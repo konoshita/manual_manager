611ef7a2ca3f8f970fc7b72b3ce60fd31f3440c3
konoshita <minglicangzhi@gmail.com>
2024-02-19 16:34:02 +0900

    Revert "画像アップロード2"
    This reverts commit 389dea2be8345c1d36cf6ba40b28e6b5534e270e.

--- app/uploaders/sentence_image_uploader.rb
+++ app/uploaders/sentence_image_uploader.rb
@@ -50,7 +50,7 @@ class SentenceImageUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-   def filename
-    " #{model . date} - #{model . title} - #{ Time . now . getutc . to_s } " 
-   end
+  # def filename
+  #   "something.jpg"
+  # end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,14 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'Google',
-    google_storage_access_key_id: ENV['google_storage_access_key_id'],
-    google_storage_secret_access_key: ENV['google_storage_secret_access_key']
-  }
-  config.fog_directory = 'photo-of-the-day'
-end
+  if Rails.env.production? 
+    config.fog_provider = 'fog/google'
+    config.fog_credentials = {
+      provider: 'Google',
+      google_storage_access_key_id: ENV['google_storage_access_key_id'],
+      google_storage_secret_access_key: ENV['google_storage_secret_access_key']
+    }
+    config.fog_directory = ENV['GCS_BUCKET']
+  else # 本番環境以外の場合はアプリケーション内にアップロード
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end
