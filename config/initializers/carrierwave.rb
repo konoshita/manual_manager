389dea2be8345c1d36cf6ba40b28e6b5534e270e
konoshita <minglicangzhi@gmail.com>
2024-02-19 16:21:30 +0900

    画像アップロード2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,9 @@
 CarrierWave.configure do |config|
-  if Rails.env.production? 
-    config.fog_provider = 'fog/google'
-    config.fog_credentials = {
-      provider: 'Google',
-      google_storage_access_key_id: ENV['google_storage_access_key_id'],
-      google_storage_secret_access_key: ENV['google_storage_secret_access_key']
-    }
-    config.fog_directory = ENV['GCS_BUCKET']
-  else # 本番環境以外の場合はアプリケーション内にアップロード
-    config.storage :file
-    config.enable_processing = false if Rails.env.test?
-  end
+  config.fog_credentials = {
+    provider: 'Google',
+    google_storage_access_key_id: ENV['google_storage_access_key_id'],
+    google_storage_secret_access_key: ENV['google_storage_secret_access_key']
+  }
+  config.fog_directory = 'photo-of-the-day'
+end
 end
